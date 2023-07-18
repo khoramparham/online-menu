396a41f6603e2e2b47aa26d589da288adc80489f
parham khoram <khoramparham@gmail.com>
2023-07-18 21:08:27 +0430

    add refresh token strategy

---
+++ src/auth/strategy/refreshJwt.strategy.ts
@@ -0,0 +1,28 @@
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { PassportStrategy } from '@nestjs/passport';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { PrismaService } from 'src/prisma/prisma.service';
+
+@Injectable()
+export class refreshJwtStrategy extends PassportStrategy(
+    Strategy,
+    'jwt-refresh',
+) {
+    constructor(config: ConfigService, private prisma: PrismaService) {
+        super({
+            jwtFromRequest: ExtractJwt.fromBodyField('refresh_token'),
+            secretOrKey: config.get('REFRESH_TOKEN_SECRET_KEY'),
+        });
+    }
+    async validate(payload: { userID: number; phone: string }) {
+        const user = await this.prisma.user.findUnique({
+            where: {
+                id: payload.userID,
+                phone: payload.phone,
+            },
+        });
+        if (!user) throw new UnauthorizedException('کاربر یافت نشد ');
+        return user;
+    }
+}
